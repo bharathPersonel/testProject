2fca786717c608a98cfc5177d47b36fd48666171
Bharath <bharath.varma@fingent.com>
2021-05-18 21:28:32 +0530

    initial commit

---
+++ TestProject 2/TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by fingent on 22/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
